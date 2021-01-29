5fb2ec744e8fe114488804f7fdc2bf8b9be8a8ad
Matt Moore <matt@mattmoore.io>
2021-01-29 10:35:13 -0600

    Add entrypoint for Scala.

--- src/main/scala/lexi/frontend/Language.scala
+++ src/main/scala/lexi/frontend/Language.scala
@@ -3,4 +3,5 @@ package lexi.frontend
 object Language extends Enumeration {
   type Language = Value
   val Kotlin: Language = Value
+  val Scala: Language = Value
 }
---
+++ src/main/scala/lexi/frontend/scala/ast/phases/LanguageAnalysis.scala
@@ -0,0 +1,8 @@
+package lexi.frontend.scala.ast.phases
+
+import lexi.ir.nodes.IrNode
+
+object LanguageAnalysis {
+  def apply(source: String): IrNode =
+    new IrNode {}
+}
