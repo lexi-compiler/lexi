239d7e366c0682fda22b9d7dc6d13ec8446b79ed
Matt Moore <matt@mattmoore.io>
2021-01-28 21:53:05 -0600

    Use function composition with ASM.

--- src/main/scala/lexi/backend/asm/ir/IrClassVisitor.scala
+++ src/main/scala/lexi/backend/asm/ir/IrClassVisitor.scala
@@ -4,23 +4,21 @@ import lexi.ir.nodes.{IrClass, IrNode}
 import org.objectweb.asm.{ClassWriter, Opcodes}
 
 object IrClassVisitor extends IrVisitor[ClassWriter] {
-  override def visit(ir: IrNode): ClassWriter = {
-    val irClass = ir.asInstanceOf[IrClass]
-
-    val writer = new ClassWriter(ClassWriter.COMPUTE_MAXS)
-
-    writer.visit(
-      Opcodes.V1_8,
-      Opcodes.ACC_PUBLIC,
-      irClass.name,
-      null,
-      "java/lang/Object",
-      null
-    )
-
-    irClass.methods.foreach(IrMethodVisitor(writer).visit(_))
-
-    writer.visitEnd()
-    writer
-  }
+  override def visit(ir: IrNode): ClassWriter =
+    (((_: IrClass) => new ClassWriter(ClassWriter.COMPUTE_MAXS))
+      andThen ((writer) => {
+        writer.visit(
+          Opcodes.V1_8,
+          Opcodes.ACC_PUBLIC,
+          ir.asInstanceOf[IrClass].name,
+          null,
+          "java/lang/Object",
+          null
+        )
+        ir.asInstanceOf[IrClass]
+          .methods
+          .foreach(IrMethodVisitor(writer).visit(_))
+        writer.visitEnd()
+        writer
+      }))(ir.asInstanceOf[IrClass])
 }
--- src/main/scala/lexi/backend/asm/ir/IrMethodVisitor.scala
+++ src/main/scala/lexi/backend/asm/ir/IrMethodVisitor.scala
@@ -10,35 +10,35 @@ object IrMethodVisitor {
 
 class IrMethodVisitor(classWriter: ClassWriter)
   extends IrVisitor[MethodVisitor] {
-  override def visit(ir: IrNode): MethodVisitor = {
-    val irFunction = ir.asInstanceOf[IrFunction]
-
-    val method = classWriter.visitMethod(
-      Opcodes.ACC_PUBLIC + Opcodes.ACC_STATIC,
-      irFunction.name,
-      "([Ljava/lang/String;)V",
-      null,
-      null
-    )
-    method.visitCode()
-    method.visitFieldInsn(
-      Opcodes.GETSTATIC,
-      "java/lang/System",
-      "out",
-      "Ljava/io/PrintStream;"
-    )
-    method.visitLdcInsn("Class compiled!")
-    method.visitMethodInsn(
-      Opcodes.INVOKEVIRTUAL,
-      "java/io/PrintStream",
-      "println",
-      "(Ljava/lang/String;)V",
-      false
-    )
-    method.visitInsn(Opcodes.RETURN)
-    method.visitMaxs(0, 0)
-    method.visitEnd()
-
-    method
-  }
+  override def visit(ir: IrNode): MethodVisitor =
+    (
+      (function: IrFunction) => {
+        val method = classWriter.visitMethod(
+          Opcodes.ACC_PUBLIC + Opcodes.ACC_STATIC,
+          function.name,
+          "([Ljava/lang/String;)V",
+          null,
+          null
+        )
+        method.visitCode()
+        method.visitFieldInsn(
+          Opcodes.GETSTATIC,
+          "java/lang/System",
+          "out",
+          "Ljava/io/PrintStream;"
+        )
+        method.visitLdcInsn("Class compiled!")
+        method.visitMethodInsn(
+          Opcodes.INVOKEVIRTUAL,
+          "java/io/PrintStream",
+          "println",
+          "(Ljava/lang/String;)V",
+          false
+        )
+        method.visitInsn(Opcodes.RETURN)
+        method.visitMaxs(0, 0)
+        method.visitEnd()
+        method
+      }
+    )(ir.asInstanceOf[IrFunction])
 }
