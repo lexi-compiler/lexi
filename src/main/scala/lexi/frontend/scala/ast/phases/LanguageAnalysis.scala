d9f0f18c43810a53ac286a1f26d59cefcd19186e
Matt Moore <matt@mattmoore.io>
2021-01-30 17:02:39 -0600

    Make package name more convenient.

--- src/main/scala/lexi/frontend/scala/ast/phases/LanguageAnalysis.scala
+++ src/main/scala/lexi/frontend/scala/ast/phases/LanguageAnalysis.scala
@@ -1,7 +1,10 @@
 package lexi.frontend.scala.ast.phases
 
 import lexi.ir.nodes.IrNode
+import dotty.tools._
 
 object LanguageAnalysis:
-  def apply(source: String): IrNode =
+  def apply(source: String): IrNode = {
+    val compiler = new dotc.Compiler
     new IrNode {}
+  }
