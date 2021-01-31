ec786d233cee85f8a9796d2e7b2d4c61947baae5
Matt Moore <matt@mattmoore.io>
2021-01-30 19:57:05 -0600

    Adding scala 3 compiler setup and context.

--- src/main/scala/lexi/Compiler.scala
+++ src/main/scala/lexi/Compiler.scala
@@ -1,10 +1,10 @@
 package lexi
 
-import lexi.frontend.kotlin.phases.LanguageAnalysis
+//import lexi.frontend.kotlin.phases.LanguageAnalysis
+import lexi.frontend.scala.phases.LanguageAnalysis
 import lexi.ir.nodes.IrNode
 import lexi.ir.phases.IrAnalysis
 
 object Compiler:
   def compile(source: String): IrNode =
-    (LanguageAnalysis.apply _
-      andThen IrAnalysis.apply)(source)
+    (LanguageAnalysis.apply _)(source)
--- src/main/scala/lexi/frontend/scala/phases/LanguageAnalysis.scala
+++ src/main/scala/lexi/frontend/scala/phases/LanguageAnalysis.scala
@@ -2,9 +2,32 @@ package lexi.frontend.scala.phases
 
 import lexi.ir.nodes.IrNode
 import dotty.tools._
+import dotty.tools.dotc.core._
+import dotty.tools.dotc.core.Contexts._
 
 object LanguageAnalysis:
   def apply(source: String): IrNode = {
+    implicit var context: Context = initialCtx
     val compiler = new dotc.Compiler
+    val run = compiler.newRun
+    run.compileFromStrings(List(source))
     new IrNode {}
+  }
+
+  protected def initialCtx: FreshContext = {
+    val base = new ContextBase {}
+    import base.settings._
+    val ctx = base.initialCtx.fresh
+    initializeCtx(ctx)
+    // when classpath is changed in ctx, we need to re-initialize to get the
+    // correct classpath from PathResolver
+    base.initialize()(using ctx)
+    ctx
+  }
+
+  protected def initializeCtx(fc: FreshContext): Unit = {
+    fc.setSetting(fc.settings.encoding, "UTF8")
+//    fc.setSetting(fc.settings.classpath, TestConfiguration.basicClasspath)
+    fc.setSetting(fc.settings.YerasedTerms, true)
+//    fc.setProperty(ContextDoc, new ContextDocstrings)
   }
