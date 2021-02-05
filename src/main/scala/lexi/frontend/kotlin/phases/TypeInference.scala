f29320e26f2c755ba62f705bdd9ab3993a95c4af
Matt Moore <matt@mattmoore.io>
2021-02-05 15:49:21 -0600

    Start working on type inference.

--- src/main/scala/lexi/frontend/kotlin/phases/TypeInference.scala
+++ src/main/scala/lexi/frontend/kotlin/phases/TypeInference.scala
@@ -1,42 +1,28 @@
 package lexi.frontend.kotlin.phases
 
-import lexi.frontend.kotlin.antlr.KotlinParser.PropertyDeclarationContext
+import lexi.frontend.kotlin.antlr.KotlinParser.{
+  FunctionDeclarationContext,
+  PropertyDeclarationContext
+}
 import lexi.frontend.kotlin.ast.{ASTNode, KtFunction, KtProperty}
 
-object TypeInference:
-  def apply(ast: ASTNode): ASTNode = ast
-//    ast match {
-//      case property: KtProperty => inferPropertyType(property)
-//      case function: KtFunction => inferFunctionType(function)
-//    }
-
-//  private def inferType(astNode: ASTNode): ASTNode = {
-//    val IntPattern = "\\d+".r
-//    val StringPattern = """^".*"$""".r
-//  }
-//
-//  private def inferPropertyType(ktProperty: KtProperty): KtProperty = {
-//    val IntPattern = "\\d+".r
-//    val StringPattern = """^".*"$""".r
-//    val inferredType = ktProperty.context
-//      .asInstanceOf[PropertyDeclarationContext]
-//      .expression
-//      .getText match {
-//      case IntPattern()    => "Int"
-//      case StringPattern() => "String"
-//    }
-//    ktProperty.copy(dataType = inferredType)
-//  }
-//
-//  private def inferFunctionType(ktFunction: KtFunction): KtFunction = {
-//    val IntPattern = "\\d+".r
-//    val StringPattern = """^".*"$""".r
-//    val inferredType = ktFunction.context
-//      .asInstanceOf[PropertyDeclarationContext]
-//      .expression
-//      .getText match {
-//      case IntPattern()    => "Int"
-//      case StringPattern() => "String"
-//    }
-//    ktFunction.copy(`type` = inferredType)
-//  }
+object TypeInference {
+  def apply(ast: ASTNode): ASTNode = {
+    val IntPattern = "\\d+".r
+    val StringPattern = """^".*"$""".r
+    ast match {
+      case property: KtProperty =>
+        val textValue = property.context
+          .asInstanceOf[PropertyDeclarationContext]
+          .expression
+          .getText
+        val inferredType = textValue match {
+          case IntPattern()    => "Int"
+          case StringPattern() => "String"
+        }
+        property.copy(dataType = inferredType)
+//      case function: KtFunction =>
+//        function.context.asInstanceOf[FunctionDeclarationContext].`type`
+    }
+  }
+}
--- src/test/scala/lexi/frontend/kotlin/phases/TypeInferenceSpec.scala
+++ src/test/scala/lexi/frontend/kotlin/phases/TypeInferenceSpec.scala
@@ -1,21 +1,21 @@
-//package lexi.frontend.kotlin.phases
-//
-//import lexi.frontend.kotlin.ast.{ASTNode, KtFile, KtProperty}
-//
-//class TypeInferenceSpec extends munit.FunSuite {
-//  private def node(ast: ASTNode): KtProperty =
-//    ast
-//      .asInstanceOf[KtFile]
-//      .topLevelObjects
-//      .head
-//      .declaration
-//      .propertyDeclaration
-//
-//  test("infers integer value") {
-//    val ast = SyntaxAnalysis("val x = 5")
-//    val typeInferredAst = TypeInference(ast)
-//    val typeInferredProperty = node(typeInferredAst)
-//    val expected = KtProperty("x", "5", "Int")
-//    assertEquals(typeInferredProperty, expected)
-//  }
-//}
+package lexi.frontend.kotlin.phases
+
+import lexi.frontend.kotlin.ast.{ASTNode, KtFile, KtProperty}
+
+class TypeInferenceSpec extends munit.FunSuite {
+  private def node(ast: ASTNode): KtProperty =
+    ast
+      .asInstanceOf[KtFile]
+      .topLevelObjects
+      .head
+      .declaration
+      .propertyDeclaration
+
+  test("infers integer value") {
+    val ast = SyntaxAnalysis("val x = 5")
+    val typeInferredAst = TypeInference(ast)
+    val typeInferredProperty = node(typeInferredAst)
+    val expected = KtProperty("x", "5", "Int")
+    assertEquals(typeInferredProperty, expected)
+  }
+}
